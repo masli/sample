0c80547008ce20cbb993c3e7d08a30d6010a2c52
mihaela <pascuta.mihaela@gmail.com>
2011-10-06 12:54:20 +0300

    added the lkayout

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,15 @@
 class PagesController < ApplicationController
   
   def home
+    @title = "Home"
   end
 
   def contact
+    @title = "About"
+  end
+
+  def about
+    @title = "Contact"
   end
 
 end
